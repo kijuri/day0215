7d8f4bbcbdf04d47911c072f96dbe2c72fb250de
unknown <romikiju1@gmail.com>
2023-02-20 19:07:31 +0900

    02-15

---
+++ js/test.js
@@ -0,0 +1,16 @@
+var user_name = "블랙자몽" ; 
+var user_num = 379 ;
+document.write('javascript이름=', user_name, '<br>') ;
+document.write('javascript숫자=', user_num) ;
+document.write('<hr size=5 color=blue>');
+
+var kor = 90 ; 
+var eng = 85 ;
+var total = kor + eng ; 
+var avg = total/2 ;
+document.write('국어=', kor, '<br>') ;
+document.write('영어=', eng, '<br>') ;
+document.write('총점=', total, '<br>') ;
+document.write('평균=', avg, '<br>') ;
+
+document.write('<hr size=5 color=blue>');
